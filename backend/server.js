03b72754f0a409997a9f963c6c078e6b3406d4fb
Mutuba <danielmutubait@gmail.com>
2024-09-02 20:30:16 +0300

    feat: Add auth context to use Node backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import notesRoutes from "./routes/notesRoutes.js";
 
@@ -8,10 +9,19 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
