2759584717630ea8ed9038544963edf688054a1c
Mutuba <danielmutubait@gmail.com>
2024-09-05 12:49:49 +0300

    chore: clean up tests

--- frontend/src/__tests__/components/NoteCard.test.jsx
+++ frontend/src/__tests__/components/NoteCard.test.jsx
@@ -1,19 +1,28 @@
-import { render, screen } from "@testing-library/react";
-import { describe, test, expect, vi } from "vitest";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
 import NoteCard from "../../components/NoteCard";
 import { NotesContext } from "../../context/NotesContext";
 import { AuthContext } from "../../context/AuthContext";
+import { deleteNote } from "../../services/notes_service";
 
-const setSelectedNote = vi.fn();
 const userToken = "random-token";
+const setSelectedNoteMock = vi.fn();
+const setNotesMock = vi.fn();
 
-const mockContext = { setSelectedNote };
-const mockToken = { userToken };
+const mockNotesContext = {
+  setSelectedNote: setSelectedNoteMock,
+  setNotes: setNotesMock,
+};
+vi.mock("../../services/notes_service", () => ({
+  deleteNote: vi.fn(() => Promise.resolve({ success: true })),
+}));
+
+const mockAuthContext = { userToken };
 
 const renderWithContext = (component) => {
   return render(
-    <AuthContext.Provider value={mockToken}>
-      <NotesContext.Provider value={mockContext}>
+    <AuthContext.Provider value={mockAuthContext}>
+      <NotesContext.Provider value={mockNotesContext}>
         {component}
       </NotesContext.Provider>
     </AuthContext.Provider>
@@ -33,11 +42,24 @@ describe("NoteCards Component", () => {
     position: { x: 605, y: 500 },
   };
 
-  test("renders the correct number of NoteCard components", () => {
+  it("renders the correct number of NoteCard components", () => {
     renderWithContext(<NoteCard note={note} />);
     expect(
-      screen.getByText(/You Don't Know JS: Scope & Closures/i)
+      screen.getByText("You Don't Know JS: Scope & Closures")
     ).toBeInTheDocument();
     expect(screen.getByTestId("note-card")).toBeInTheDocument();
+  });
+
+  it("deletes the note when the delete button is clicked", async () => {
+    renderWithContext(<NoteCard note={note} />);
+
+    const deleteButton = screen.getByTestId("delete-button");
+    fireEvent.click(deleteButton);
+
+    await waitFor(() => {
+      expect(deleteNote).toHaveBeenCalledWith(note._id, userToken);
+    });
+
+    expect(setNotesMock).toHaveBeenCalled();
   });
 });
